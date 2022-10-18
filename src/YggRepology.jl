0daa6ea93c47869602fbdfb6987746fa87675a86
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-10-18 07:20:57 +0000

    Update code

--- src/YggRepology.jl
+++ src/YggRepology.jl
@@ -6,12 +6,8 @@ using GitHub
 using Base64
 using Chain
 using TOML
-
-myauth = GitHub.authenticate(ENV["GITHUB_TOKEN"])
-binary_repositories = repos("JuliaBinaryWrappers"; auth=myauth)
-
-repository_list = binary_repositories[1]
-repository = repository_list[1]
+using DataFrames
+using DataFrameMacros
 
 function get_file(repository_name, filename, auth)
     @chain repository_name begin
@@ -51,7 +47,7 @@ function extract_readme_metadata(readme_text)
     return Dict("source_url" => source_url, "recipe_url" => recipe_url)
 end
 
-function get_readme_metadata(repository_name, auth)
+function get_readme_metadata(repository_name::String, auth)
     try
         readme_text = get_readme(repository_name, auth)
         return extract_readme_metadata(readme_text)
@@ -60,29 +56,33 @@ function get_readme_metadata(repository_name, auth)
     end
 end
 
-function get_toml_metadata(repository_name, auth)
+function get_toml_metadata(repository_name::String, auth)
     try
         project_toml = get_toml_file(repository_name, "Project.toml", myauth)
         artifacts_toml = get_toml_file(repository_name, "Artifacts.toml", myauth)
+
+        binary_name = @chain project_toml["name"] replace("_jll" => "")
+        version = project_toml["version"]
+    
+        # TODO: loop over all platforms to get platform metadata
+        # platform_triple = @chain artifacts_toml[binary_name][1] begin
+        #    "$(_["arch"])-$(_["os"])-$(_["libc"])"
+        # end
+    
+        return Dict("binary_name" => binary_name, "version" => version)
     catch
         return Dict()
     end
-    binary_name = @chain project_toml["name"] replace("_jll" => "")
-    version = project_toml["version"]
 
-    # TODO: loop over all platforms to get platform metadata
-    # platform_triple = @chain artifacts_toml[binary_name][1] begin
-    #    "$(_["arch"])-$(_["os"])-$(_["libc"])"
-    # end
-
-    return Dict("binary_name" => binary_name, "version" => version)
 end
 # Sketch in requirements https://repology.org/docs/requirements
 
-function get_binary_info(repository_name, auth)
+function get_binary_info(repository::Repo, auth)
+    repository_name = repository.full_name
     return Dict(
         get_readme_metadata(repository_name, auth)...,
         get_toml_metadata(repository_name, auth)...,
+        :update_date => repository.updated_at
     )
 end
 
@@ -96,6 +96,14 @@ function gather_all_binary_info()
     return full_binary_metadata
 end
 
+drop_url_from_list(x) = length(x) != 1 ? x : replace(x[1], r".*(https?://.*)" => s"\1")
+
+myauth = GitHub.authenticate(ENV["GITHUB_TOKEN"])
+binary_repositories = repos("JuliaBinaryWrappers"; auth=myauth)
+
+repository_list = binary_repositories[1]
+repository = repository_list[1]
+
 repository_list_ = repository_list[1:100]
 repository = repository_list[1]
 repository_name = repository.full_name
@@ -108,9 +116,9 @@ full_binary_metadata = [
 
 df = DataFrame(full_binary_metadata)
 
-drop_list(x) = length(x) != 1 ? x : replace(x[1], r".*(https?://.*)" => s"\1")
+
 
 @chain df begin
-    @transform :source_url = drop_list(:source_url)
-    @subset !(source_url isa String)
+    @transform :source_url = drop_url_from_list(:source_url)
+    @subset !(:source_url isa String)
 end
