40e623d9a3f4cbd085a66a2483654ed8cc36432b
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-10-23 12:47:53 +0200

    Expand tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Test
 using YggRepology
+using GitHub
 
 @testset "YggRepology utils" begin
     @test YggRepology.get_toml_file("JuliaBinaryWrappers/DuckDB_jll.jl", "Project.toml", "main")["version"] >= "0.2"
@@ -18,25 +19,16 @@ using YggRepology
     
     @test YggRepology.get_toml_metadata("JuliaBinaryWrappers/DuckDB_jll.jl", "main")[:version] >= "0.2.5"
 
-    # drop_url_from_list([""]
+    @test YggRepology.drop_url_from_list(["git repository: https://github.com/duckdb/duckdb.git (revision: `7c111322de1095436350f95e33c5553b09302165`)"]) == "https://github.com/duckdb/duckdb.git"
+
+    @test [keys(YggRepology.get_binary_info(GitHub.Repo(; full_name = "JuliaBinaryWrappers/DuckDB_jll.jl", name = "DuckDB_jll.jl", default_branch = "main", updated_at = Date("2021-01-01"), pushed_at = Date("2021-01-01"))))...] == [:update_date, :pushed_at, :binary_name, :version, :source_url, :recipe_url]
 end
 
-
-    # myauth = GitHub.authenticate(ENV["GITHUB_TOKEN"])
-    # binary_repositories = repos("JuliaBinaryWrappers"; auth=myauth)
-
-    # repository_list = binary_repositories[1]
-    # repository = repository_list[1]
-
-    # repository_list_ = repository_list[101:200]
-    # repository = repository_list[1]
-    # repository_name = repository.full_name
-
-    # get_binary_info(repository)
-    # full_binary_metadata = [
-    #     get_binary_info(repository) for
-    #     repository in repository_list if repository.updated_at > Date("2021-01-01")
-    # ]
+@testset "YggRepology Integration Tests" begin
+    binary_info = YggRepology.gather_all_binary_info()
+    @test binary_info isa list
+    @test length(binary_info) > 500
 
     # export_all_binary_info()
-    # get_all_binary_info()
+
+end
