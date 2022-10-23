95785b62ac926a52a8d380357e0d24b878c87f50
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-10-23 13:43:02 +0200

    Tweak source_url check

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,13 +22,18 @@ using GitHub
     @test YggRepology.drop_url_from_list(["git repository: https://github.com/duckdb/duckdb.git (revision: `7c111322de1095436350f95e33c5553b09302165`)"]) == "https://github.com/duckdb/duckdb.git"
 
     @test [keys(YggRepology.get_binary_info(GitHub.Repo(; full_name = "JuliaBinaryWrappers/DuckDB_jll.jl", name = "DuckDB_jll.jl", default_branch = "main", updated_at = Date("2021-01-01"), pushed_at = Date("2021-01-01"))))...] == [:update_date, :pushed_at, :binary_name, :version, :source_url, :recipe_url]
+
+    # @test get_patch_directories(["git repository:
 end
 
 @testset "YggRepology Integration Tests" begin
-    binary_info = YggRepology.gather_all_binary_info(maxrepos=10)
-    @test binary_info isa list
-    @test length(binary_info) > 500
+    full_binary_metadata = YggRepology.gather_all_binary_info(maxrepos=10)
+    @test full_binary_metadata isa list
+    @test length(full_binary_metadata) == 10 500
 
     # export_all_binary_info()
 
 end
+
+
+# full_binary_metadata = YggRepology.gather_all_binary_info()
